c26e1ae692b31b7ef598aaf1b011b1178615238c
Logesh-Kaavyan <logeshkaavyan.pks2022@vitstudent.ac.in>
2024-07-02 11:44:17 +0530

    Home Fragment-UI and Corrections

--- app/src/main/java/com/example/travelapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/travelapp/fragment/HomeFragment.kt
@@ -11,33 +11,32 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.travelapp.databinding.ActivityHomeFragmentBinding
-//import com.example.travelapp.viewmodel.DiaryViewModel
+import com.example.travelapp.viewmodel.DiaryViewModel
 
 class HomeFragment : Fragment() {
     private var binding: ActivityHomeFragmentBinding? = null
     private var adapter: HomeRecyclerViewAdapter? = null
     private var layoutManager: RecyclerView.LayoutManager? = null
-    /*private var diaryViewModel: DiaryViewModel? = null
+    private var diaryViewModel: DiaryViewModel? = null
     override fun onCreate(arg: Bundle?) {
         super.onCreate(arg)
-        diaryViewModel =
-            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application).create<DiaryViewModel>(
+        diaryViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application).create<DiaryViewModel>(
                 DiaryViewModel::class.java
             )
         diaryViewModel!!.SynchronizeGetData()
-    }*/
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+        savedInstanceState: Bundle?,
+    ): View? {
         // Inflate the View for this fragment
         binding = ActivityHomeFragmentBinding.inflate(inflater, container, false)
-        val view: View = binding!!.root
+        val view: View = binding!!.getRoot()
 
-        /*val diaryList = diaryViewModel!!.allId
-        adapter = HomeRecyclerViewAdapter(diaryViewModel!!, diaryList!!.value as List<Int>?)
+        val diaryList = diaryViewModel!!.allId
+        adapter = HomeRecyclerViewAdapter(diaryViewModel!!, diaryList.value)
         //this just creates a line divider between rows
         binding!!.recyclerView.addItemDecoration(
             DividerItemDecoration(
@@ -45,12 +44,23 @@ class HomeFragment : Fragment() {
                 LinearLayoutManager.VERTICAL
             )
         )
-        binding!!.recyclerView.adapter = adapter
+        binding!!.recyclerView.setAdapter(adapter)
 
         layoutManager = LinearLayoutManager(activity)
-        binding!!.recyclerView.layoutManager = layoutManager
-        diaryList.observe(viewLifecycleOwner
-        ) */{ }
+        binding!!.recyclerView.setLayoutManager(layoutManager)
+        diaryList.observe(viewLifecycleOwner, object : Observer<List<Int?>?> {
+            override fun onChanged(value: List<Int?>?) {
+                adapter = HomeRecyclerViewAdapter(diaryViewModel!!, value)
+                //this just creates a line divider between rows
+                binding!!.recyclerView.addItemDecoration(
+                    DividerItemDecoration(
+                        activity,
+                        LinearLayoutManager.VERTICAL
+                    )
+                )
+                binding!!.recyclerView.setAdapter(adapter)
+            }
+        })
         return view
     }
 
@@ -58,5 +68,4 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         binding = null
     }
-
 }
--- app/src/main/java/com/example/travelapp/fragment/HomeRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/travelapp/fragment/HomeRecyclerViewAdapter.kt
@@ -9,10 +9,14 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.travelapp.R
 import com.example.travelapp.databinding.HomeCardLayoutBinding
+import com.example.travelapp.entity.DiaryEntry
+import com.example.travelapp.viewmodel.DiaryViewModel
 
-
-abstract class HomeRecyclerViewAdapter(
+class HomeRecyclerViewAdapter(
+    private val diaryViewModel: DiaryViewModel,
+    private val diaryListId: List<Int?>?
 ) : RecyclerView.Adapter<HomeRecyclerViewAdapter.ViewHolder?>() {
+
     override fun onCreateViewHolder(
         parent: ViewGroup, viewType: Int
     ): ViewHolder {
@@ -23,7 +27,43 @@ abstract class HomeRecyclerViewAdapter(
 
     // this method binds the view holder created with data that will be displayed
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val idDiary = diaryListId?.get(position)
+        if (idDiary != null) {
+            val diary = diaryViewModel.findDiarybyId(idDiary)
+            if (diary != null) {
+                // Set the image based on the diary ID modulo 10
+//                when (diary.id!! % 10) {
+//                    0 -> viewHolder.binding.image.setImageResource(R.drawable.i1)
+//                    1 -> viewHolder.binding.image.setImageResource(R.drawable.i2)
+//                    2 -> viewHolder.binding.image.setImageResource(R.drawable.i3)
+//                    3 -> viewHolder.binding.image.setImageResource(R.drawable.i4)
+//                    4 -> viewHolder.binding.image.setImageResource(R.drawable.i5)
+//                    5 -> viewHolder.binding.image.setImageResource(R.drawable.i6)
+//                    6 -> viewHolder.binding.image.setImageResource(R.drawable.i7)
+//                    7 -> viewHolder.binding.image.setImageResource(R.drawable.i8)
+//                    8 -> viewHolder.binding.image.setImageResource(R.drawable.i9)
+//                    9 -> viewHolder.binding.image.setImageResource(R.drawable.i10)
+//                }
 
+                // Updating UI with diary details
+                viewHolder.binding.logixTiltle.text = diary.title
+                viewHolder.binding.logixIntroduce.text = diary.description
+                viewHolder.binding.cardView.setOnClickListener { v ->
+                    val arg = Bundle().apply {
+                        putString("content", diary.id.toString())
+                    }
+                    replaceFragment(arg, v)
+                }
+            } else {
+                // Handle the case where diary is null
+                viewHolder.binding.logixTiltle.text = "Diary not found"
+                viewHolder.binding.logixIntroduce.text = ""
+            }
+        } else {
+            // Handle the case where idDiary is null
+            viewHolder.binding.logixTiltle.text = "ID not available"
+            viewHolder.binding.logixIntroduce.text = ""
+        }
     }
 
     private fun replaceFragment(arg: Bundle, view: View) {
@@ -34,6 +74,12 @@ abstract class HomeRecyclerViewAdapter(
         val navController = navHostFragment!!.navController
         navController.navigate(R.id.logixContentFragment, arg)
     }
+
+    override fun getItemCount(): Int {
+        if (diaryListId == null) return 0
+        return diaryListId.size
+    }
+
     class ViewHolder(val binding: HomeCardLayoutBinding) : RecyclerView.ViewHolder(
         binding.root
     )
